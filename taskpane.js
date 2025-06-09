4f4b3b3f1c7ed553aa3c0d0188e8a0576e9e01a2
317jamtay317 <jamtay317@gmail.com>
2025-06-09 08:03:24 -0400

    excel adding displaying in the taskpain

--- taskpane.js
+++
@@ -1,7 +0,0 @@
-function App() {
-  return React.createElement('h1', null, 'Hello World');
-}
-
-Office.onReady(function() {
-  ReactDOM.render(React.createElement(App), document.getElementById('root'));
-});
